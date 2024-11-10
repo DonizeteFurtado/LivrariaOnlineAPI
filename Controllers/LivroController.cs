ba22a4c114f1baded9e27f2be268809f0a3ee15e
Carlos Furtado <fonizetefurado24@gmail.com>
2024-11-10 13:49:11 -0300

    Add project files.

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,34 @@
+﻿using LivrariaOnlineAPI.Comunicacao.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivrariaOnlineAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LivroController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CriarLivro()
+    {
+        //var Response = new RequestLivro();
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult VerLivro()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult AtualizaLivro()
+    {
+        return BadRequest();
+    }
+
+    [HttpDelete]
+    public IActionResult DeletaLivro()
+    {
+        return BadRequest();
+    }
+}
