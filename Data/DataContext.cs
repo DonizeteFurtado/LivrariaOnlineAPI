ed9778d08a109f5c19a365b0947cd3a5b827bc5b
Donizete Furtado <fonizetefurtado24@gmail.com>
2024-11-11 21:09:38 -0300

    Ajuste para usar o banco de dados na aplicacao

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using LivrariaOnlineAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LivrariaOnlineAPI.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    { }
+    
+    public DbSet<LivroModel> Livros { get; set; }
+}
---
+++ Models/LivroModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LivrariaOnlineAPI.Models;
+
+public class LivroModel
+{
+    public required string Id {  get; set; }
+    public required string Titulo { get; set; }
+    public required string Autor {  get; set; }
+    public required string Genero { get; set; }
+    public required decimal Preco { get; set; }  
+    public required int QtdEstoque { get; set; }
+}
